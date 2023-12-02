847ef96bf3295d94649c9b3c0a869dfcfc706ab8
Espen Røvik Larsen <>
2023-12-02 23:08:24 +0100

    add symbolicate ios

---
+++ SymolicateIos/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+using ELFSharp.MachO;
+
+var path = @"C:\Users\erl0\Downloads\naturoppdrag-ci-build-ios-2.0.36\Naturoppdrag";
+var path2 = @"C:\Users\erl0\Downloads\stacktrace.txt";
+var outputPath = @"C:\Users\erl0\Downloads\stacktrace.fixed.txt";
+
+var loadAddress = 0x000102be0000;
+
+var machOReader = MachOReader.Load(path);
+
+var dylib = machOReader
+    .GetCommandsOfType<Dylib>()
+    .ToList();
+
+var uuid = machOReader
+    .GetCommandsOfType<UUID>()
+    .ToList();
+
+var entryPoint = machOReader
+    .GetCommandsOfType<EntryPoint>()
+    .ToList();
+
+var segment = machOReader
+    .GetCommandsOfType<Segment>()
+    .ToList();
+
+var symbols2 = machOReader
+    .GetCommandsOfType<SymbolTable>()
+    .Single()
+    .Symbols;
+
+var map = symbols2
+    .GroupBy(x => x.Value)
+    .ToDictionary(x => x.Key);
+
+var a = 0x208b96724 - loadAddress;
+if (map.TryGetValue(a, out var symbols22))
+{
+}
+
+// debug_meta -> images -> debug_id, code_file, image_addr
+
+var ass = symbols2.Where(x => string.Equals("_sqlite3VdbeParameterIndex", x.Name, StringComparison.Ordinal)).ToList();
+
+
+var stringBuilder = new StringBuilder();
+
+foreach (var line in File.ReadLines(path2))
+{
+    if (StackFrame.FromString(line) is { } stackFrame && string.Equals("Naturoppdrag", stackFrame.Module, StringComparison.Ordinal))
+    {
+        // TODO: handle multiple symbols with the same offset
+        string outputLine;
+        if (map.TryGetValue(stackFrame.Offset - loadAddress, out var symbols))
+            outputLine = line.Replace(stackFrame.Symbol, symbols.First().Name);
+        else
+            outputLine = line;
+
+        stringBuilder.AppendLine(outputLine);
+    }
+    else
+    {
+        stringBuilder.AppendLine(line);
+    }
+}
+
+File.WriteAllText(outputPath, stringBuilder.ToString());
---
+++ SymolicateIos/StackFrame.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+[DebuggerDisplay("{Module} {Offset} {Symbol}")]
+public partial struct StackFrame
+{
+    public string Module { get; private init; }
+    public string Symbol { get; private init; }
+    public long Offset { get; private init; }
+
+    public static StackFrame? FromString(string line)
+    {
+        // Parses lines from a stack trace that has been decorated with IL offsets and
+        // method tokens.
+        //
+        // For example:
+        // 0   Naturoppdrag   0x208b96724         <unknown> + 204
+        // 1   Naturoppdrag   0x208b5e1f4[inlined] < unknown > +356
+        //
+        // The token consists of one byte for the type (only methods, 0x06, is
+        // supported for now) and three bytes for the token value.
+
+        var match = LineRegex().Match(line);
+
+        if (match is { Success: true, Groups.Count: 6 })
+        {
+            var module = match.Groups[2].Value;
+            var offset = Convert.ToInt64(match.Groups[3].Value, 16);
+            var symbol = match.Groups[4].Value;
+            var offset2 = Convert.ToInt64(match.Groups[5].Value, 10);
+
+            return new StackFrame
+            {
+                Module = module,
+                Offset = offset - offset2,
+                Symbol = symbol
+            };
+        }
+
+        return null;
+    }
+
+    [GeneratedRegex(@"(\d+)\s+(\S+)\s+(0x[0-9A-Fa-f]+)\s+(\S+)\s+\+\s+(\d+)")]
+    private static partial Regex LineRegex();
+}
